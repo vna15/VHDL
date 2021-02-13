7b28e37d804fc01e377c330b955f07d69e49e720
vna15 <30414490+vna15@users.noreply.github.com>
2021-02-13 18:37:41 -0300

    Update decode.vhd

--- Banco de registradores/decode.vhd
+++ Banco de registradores/decode.vhd
@@ -9,36 +9,26 @@ end decodificador;
 
 architecture ckt of decodificador is
 
-   signal aux_2 : std_logic_vector(15 downto 0);
-	signal aux : std_logic_vector(7 downto 0);
+   signal aux_2 : std_logic_vector(15 downto 0);	
 	
 	begin
 	
-	aux(0) <= not(X(0)) and not(X(1));
-   aux(1) <= X(0) and not(X(1));
-   aux(2) <= not(X(0)) and X(1);
-   aux(3) <= X(0) and X(1);
-	aux(4) <= not(X(2)) and not(X(3));
-   aux(5) <= X(2) and not(X(3));
-   aux(6) <= not(X(2)) and X(3);
-   aux(7) <= X(2) and X(3);
-	
-	aux_2(0) <= en and aux(0) and aux(4);
-	aux_2(1) <= en and aux(0) and aux(5);
-	aux_2(2) <= en and aux(0) and aux(6);
-	aux_2(3) <= en and aux(0) and aux(7);
-	aux_2(4) <= en and aux(1) and aux(4);
-	aux_2(5) <= en and aux(1) and aux(5);
-	aux_2(6) <= en and aux(1) and aux(6);
-	aux_2(7) <= en and aux(1) and aux(7);
-	aux_2(8) <= en and aux(2) and aux(4);
-	aux_2(9) <= en and aux(2) and aux(5);
-	aux_2(10) <= en and aux(2) and aux(6);
-	aux_2(11) <= en and aux(2) and aux(7);
-	aux_2(12) <= en and aux(3) and aux(4);
-	aux_2(13) <= en and aux(3) and aux(5);
-	aux_2(14) <= en and aux(3) and aux(6);
-	aux_2(15) <= en and aux(3) and aux(7);
+	aux_2(0) <= en and not(X(3)) and not(X(2)) and not(X(1)) and not(X(0));
+	aux_2(1) <= en and not(X(3)) and not(X(2)) and not(X(1)) and X(0);
+	aux_2(2) <= en and not(X(3)) and not(X(2)) and X(1) and not(X(0));
+	aux_2(3) <= en and not(X(3)) and not(X(2)) and X(1) and X(0);
+	aux_2(4) <= en and not(X(3)) and X(2) and not(X(1)) and not(X(0));
+	aux_2(5) <= en and not(X(3)) and X(2) and not(X(1)) and X(0);
+	aux_2(6) <= en and not(X(3)) and X(2) and X(1) and not(X(0));
+	aux_2(7) <= en and not(X(3)) and X(2) and X(1) and X(0);
+	aux_2(8) <= en and X(3) and not(X(2)) and not(X(1)) and not(X(0));
+	aux_2(9) <= en and X(3) and not(X(2)) and not(X(1)) and X(0);
+	aux_2(10) <= en and X(3) and not(X(2)) and X(1) and not(X(0));
+	aux_2(11) <= en and X(3) and not(X(2)) and X(1) and X(0);
+	aux_2(12) <= en and X(3) and X(2) and not(X(1)) and not(X(0));
+	aux_2(13) <= en and X(3) and X(2) and not(X(1)) and X(0);
+	aux_2(14) <= en and X(3) and X(2) and X(1) and not(X(0));
+	aux_2(15) <= en and X(3) and X(2) and X(1) and X(0);
 	
 	Q <= aux_2;
 	
